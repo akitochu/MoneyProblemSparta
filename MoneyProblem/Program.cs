c07dc4b2df550fa2041137247565a2b13cd53866
DESKTOP-DJK1QF7\akito <akitochu@gmail.com>
2023-04-19 11:31:23 +0100

    create money problem project and unit test files

---
+++ MoneyProblem/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace MoneyProblem
+{
+    public class Program
+    {
+        //static void Main(string[] args)
+        //{
+        //    Console.WriteLine("Hello, World!");
+        //}
+
+        static Dictionary<string, int> MoneyProblem(double value)
+        {
+            Dictionary<string, int> output = new Dictionary<string, int>();
+            return output;
+        } 
+    }
+}
---
+++ MoneyProblemTests/UnitTest1.cs
@@ -0,0 +1,18 @@
+using MoneyProblem;
+
+namespace MoneyProblemTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
