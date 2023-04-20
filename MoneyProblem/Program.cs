590d59131a3117e4e7e1aabe1c9641c37c0de2f9
ALewis <ALewis@spartaglobal.com>
2023-04-20 10:31:29 +0100

    refactored method to foreach loop, minus demoninations from value

--- MoneyProblem/Program.cs
+++ MoneyProblem/Program.cs
@@ -4,13 +4,30 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            decimal amount = 5.47M;
+            Dictionary<string, int> result = MoneyProblem(amount);
+            foreach (KeyValuePair<string, int> kvp in result)
+            {
+                Console.WriteLine("{0}: {1}", kvp.Key, kvp.Value);
+            }
         }
 
-        public static Dictionary<string, int> MoneyProblem(double value)
+        public static Dictionary<string, int> MoneyProblem(decimal value)
         {
+            if (value < 0) throw new ArgumentException("Cant be less than zero");
             Dictionary<string, int> output = new Dictionary<string, int>();
+            decimal[] denominations = { 20M, 10M, 5M, 2M, 1M, 0.5M, 0.2M, 0.1M, 0.05M, 0.02M, 0.01M };
+            foreach (decimal denomination in denominations)
+            {
+                int count = (int)(value / denomination);
+                if (count > 0)
+                {
+                    output.Add(denomination.ToString(), count);
+                    value -= count * denomination;
+                }
+            }
             return output;
-        } 
+        }
+
     }
 }
--- MoneyProblemTests/UnitTest1.cs
+++ MoneyProblemTests/UnitTest1.cs
@@ -6,26 +6,25 @@ namespace MoneyProblemTests
     public class Tests
     {
         [TestCase(5.47)]
-        public void GivenAValidInput_MoneyProblem_ReturnsCorrectOutput(double value)
+        public void GivenAValidInput_MoneyProblem_ReturnsCorrectOutput(decimal value)
         {
             Dictionary<string, int> output = new Dictionary<string, int>();
-            output.Add("£2", 2);
-            output.Add("£1", 1);
-            output.Add("20p", 2);
-            output.Add("5p", 1);
-            output.Add("2p", 1);
+            output.Add("5", 1);
+            output.Add("0.2", 2);
+            output.Add("0.05", 1);
+            output.Add("0.02", 1);
 
             Assert.That(MoneyProblem.Program.MoneyProblem(value), Is.EqualTo(output));
         }
         [TestCase(0)]
-        public void GivenAZero_MoneyProblem_ReturnsEmptyDictionary(double value)
+        public void GivenAZero_MoneyProblem_ReturnsEmptyDictionary(decimal value)
         {
             Dictionary<string, int> output = new Dictionary<string, int>();
 
             Assert.That(MoneyProblem.Program.MoneyProblem(value), Is.EqualTo(output));
         }
         [TestCase(-5.23)]
-        public void GivenANegitve_MoneyProblem_ReturnsArgumentException(double value)
+        public void GivenANegitve_MoneyProblem_ReturnsArgumentException(decimal value)
         {
             Assert.That(() => MoneyProblem.Program.MoneyProblem(value), Throws.TypeOf<ArgumentException>());
         }
